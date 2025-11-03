8a51266c6dd09a1224e14b00f161c6e6ee7772e8
Thugi-98 <kimjh4533@gmail.com>
2025-11-03 09:57:20 +0900

    11. 짝수와 홀수

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,11 @@
 class Solution {
-    public double solution(int[] numbers) {
-        double answer = 0;
-        int sum = 0;
-        int i;
+    public String solution(int num) {
+        String answer = "";
 
-        // 배열 길이 검증
-        if (numbers.length >= 1 && numbers.length <= 100) {
-            for (i = 0; i < numbers.length; i++) {
-
-                // 배열값 범위 검증
-                if (numbers[i] >= 0 && numbers[i] <= 1000)
-                    sum += numbers[i];
-                else throw new RuntimeException("배열 값이 범위를 벗어났습니다.");
-            }
-
-        } else throw new RuntimeException("배열 길이가 범위를 벗어났습니다.");
-
-        answer = (double) sum / i;
+        if (num % 2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
 
         return answer;
     }
