03e298a4c4517a3e61907c0d9109ce83f08d769c
Thugi-98 <kimjh4533@gmail.com>
2025-11-03 09:51:08 +0900

    10. 배열의 평균값

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,22 @@
 class Solution {
-    public static int solution(int n) {
-        int answer = 0;
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+        int i;
 
-        if (n > 0 && n <= 1000) {
-            for (int i = 0; i <= n; i++) {
-                if (i % 2 == 0)
-                    answer += i;
+        // 배열 길이 검증
+        if (numbers.length >= 1 && numbers.length <= 100) {
+            for (i = 0; i < numbers.length; i++) {
+
+                // 배열값 범위 검증
+                if (numbers[i] >= 0 && numbers[i] <= 1000)
+                    sum += numbers[i];
+                else throw new RuntimeException("배열 값이 범위를 벗어났습니다.");
             }
-        }
+
+        } else throw new RuntimeException("배열 길이가 범위를 벗어났습니다.");
+
+        answer = (double) sum / i;
 
         return answer;
     }
