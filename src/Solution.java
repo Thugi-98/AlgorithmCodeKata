b2877bbae224fd4e9b361178c51277681dfae290
Thugi-98 <kimjh4533@gmail.com>
2025-11-04 10:02:26 +0900

    12. 평균 구하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,20 @@
 class Solution {
-    public String solution(int num) {
-        String answer = "";
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        int i;
 
-        if (num % 2 == 0)
-            answer = "Even";
-        else
-            answer = "Odd";
+        if (arr.length >= 1 && arr.length <= 100) {
+            for (i = 0; i < arr.length; i++) {
+
+                if (arr[i] >= -10000 && arr[i] <= 10000) {
+                    sum += arr[i];
+                } else throw new RuntimeException("배열 값이 범위를 벗어났습니다.");
+            }
+
+        } else throw new RuntimeException("배열 길이가 범위를 벗어났습니다.");
+
+        answer = (double) sum / i;
 
         return answer;
     }
