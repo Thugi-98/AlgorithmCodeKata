5112471f0a2ed8a9ac9af654ec2d3c541cc45e3b
Thugi-98 <kimjh4533@gmail.com>
2025-11-10 10:07:11 +0900

    15. 나머지가 1이 되는 수 찾기

--- src/Solution.java
+++ src/Solution.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        if (n >= 0 && n <= 3000) {
-            for (int i = 1; i <= n; i++) {
-                if (n % i == 0) answer += i;
+        if (n >= 3 && n <= 1000000) {
+            for (int i = n; i > 0; i--) {
+                if (n % i == 1) answer = i;
             }
         }
 
