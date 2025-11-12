d4d8f30397c1d7d74fb4a6f321fdf7fd7aaec051
Thugi-98 <kimjh4533@gmail.com>
2025-11-12 10:00:55 +0900

    16. x만큼 간격이 있는 n개의 숫자

---
+++ src/alg016/Solution.java
@@ -0,0 +1,17 @@
+package alg016;
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+        answer = new long[n];
+        int ans = x;
+
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                answer[i] = ans;
+                ans += x;
+            }
+        } else throw new RuntimeException("n이 자연수가 아닙니다.");
+        return answer;
+    }
+}
